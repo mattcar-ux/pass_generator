6ef7d67c5929c20a34aad155e5bcc3d88d67343b
mattcar-ux <matteo.cardi@hotmail.it>
2024-12-04 19:50:27 +0100

    aggiunti caratteri e introdotta variabile

--- pass_generator.py
+++ pass_generator.py
@@ -1,4 +1,8 @@
 import random
+caratteri_pw = "a b c d e f g h i j k l m n o p q r s t u v w x y z
+                A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+                0 1 2 3 4 5 6 7 8 9
+                ? ! £ $ % & / ( ) ^ + - _ < > # @ [ ] { }"
 while True:
     try:
         input_utente = int(input("Determina la lunghezza della tua password (solo numeri interi): "))
@@ -6,8 +10,8 @@ while True:
     except ValueError: pass
 password_list = []
 password = ""
-for i in range (0, input_utente):
-    password_list = password_list + random.sample(("a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 0 1 2 3 4 5 6 7 8 9 ? ! £ $ % & / ( ) ^ + # @".split()), k=1)
+for i in range(0, input_utente):
+    password_list = password_list + random.sample(caratteri_pw.split(), k=1)
 for lettera in password_list:
     password = password + lettera
 print(password)

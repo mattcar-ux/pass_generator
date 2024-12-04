7fe582d06042f8f8f03415ec83e1bf3c58887232
mattcar-ux <matteo.cardi@hotmail.it>
2024-12-04 20:35:24 +0100

    semplificata eliminando lista

--- pass_generator.py
+++ pass_generator.py
@@ -1,17 +1,14 @@
 import random
-caratteri_pw = "a b c d e f g h i j k l m n o p q r s t u v w x y z
-                A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
-                0 1 2 3 4 5 6 7 8 9
-                ? ! £ $ % & / ( ) ^ + - _ < > # @ [ ] { }"
+caratteri_pw = " a b c d e f g h i j k l m n o p q r s t u v w x y z "\
+                " A B C D E F G H I J K L M N O P Q R S T U V W X Y Z "\
+                " ? ! £ $ % & / ^ + * § ° € = - _ < > # @ [ ] { } ( ) "\
+                " 0 1 2 3 4 5 6 7 8 9 ' | \ "
 while True:
     try:
         input_utente = int(input("Determina la lunghezza della tua password (solo numeri interi): "))
         break
     except ValueError: pass
-password_list = []
 password = ""
 for i in range(0, input_utente):
-    password_list = password_list + random.sample(caratteri_pw.split(), k=1)
-for lettera in password_list:
-    password = password + lettera
+    password += random.choice(caratteri_pw.split())
 print(password)
